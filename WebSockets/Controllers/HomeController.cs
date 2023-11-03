f9aba3562fa6f83c40e4203b9a574ff1525bdab4
amrali21 <amoormoneer@gmail.com>
2023-11-03 16:46:20 +0200

    init

---
+++ WebSockets/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using WebSockets.Models;
+
+namespace WebSockets.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private string currentStatus = string.Empty;
+        WebSocket socket;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("/ws")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("/ws")]
+        public async Task GetSocket()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                socket = webSocket;
+                await UpdateStatus();
+                //await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        private async Task sendMessage(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public async Task UpdateStatus()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                await Task.Delay(1000);
+                await sendMessage($"hello world {i}");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
